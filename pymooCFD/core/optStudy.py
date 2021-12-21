5edb1b9bdeec68ecf1e309dadaee081f04f74c0c
gmclove <george.love077@gmail.com>
2021-12-21 16:16:51 -0500

    import fix

--- pymooCFD/core/optStudy.py
+++ pymooCFD/core/optStudy.py
@@ -8,8 +8,8 @@ import numpy as np
 import subprocess
 from threading import Thread
 import copy
+import shutil
 # import h5py
-# import shutil
 # import matplotlib.pyplot as plt
 
 from pymooCFD.util.sysTools import emptyDir, copy_and_overwrite
