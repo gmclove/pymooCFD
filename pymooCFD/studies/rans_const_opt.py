ef42931c341d7b8a7e1343e9d2a363d549143c01
gmclove <george.love077@gmail.com>
2022-04-12 12:22:57 -0400

    exec_test update

--- pymooCFD/studies/rans_const_opt.py
+++ pymooCFD/studies/rans_const_opt.py
@@ -17,8 +17,8 @@ def exec_test():
         alg = study.get_algorithm(n_gen=2, pop_size=3,
                                   n_offsprings=2)
         opt_run = study.new_run(alg, prob)
-    opt_run.test_case.run()
-    opt_run.run_bnd_cases()
+    # opt_run.test_case.run()
+    # opt_run.run_bnd_cases()
     opt_run.run()
 
 def exec():
