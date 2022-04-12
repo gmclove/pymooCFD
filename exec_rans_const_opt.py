5bdbe0858e7adf53cb64bc131f1588a30c526e5a
gmclove <george.love077@gmail.com>
2022-04-12 18:56:17 -0400

    exec rans const opt

--- exec_rans_const_opt.py
+++ exec_rans_const_opt.py
@@ -5,19 +5,19 @@ import os
 def main():
     study = MinimizeCFD(RANS_k_eps)
     path = os.path.join(study.abs_path, 'best_k-e_standard')
-    RANS_k_eps(path, [0.09, 30_000])
-    xl = [0.09*0.8, 4_000]
-    xu = [0.09*1.2, 30_000]
-    prob = study.get_problem(xl, xu)
-    alg = study.get_algorithm(n_gen=20, pop_size=50, n_offsprings=10)
-    opt_run = study.new_run(alg, prob)
+    RANS_k_eps(path, [0.09, 30_000]).run()
+    # xl = [0.09*0.8, 4_000]
+    # xu = [0.09*1.2, 30_000]
+    # prob = study.get_problem(xl, xu)
+    # alg = study.get_algorithm(n_gen=20, pop_size=50, n_offsprings=10)
+    # opt_run = study.new_run(alg, prob)
     # print(study.get_logger())
     # print(study.opt_runs)
     # print(study.__dict__)
     opt_runs = study.opt_runs
     print(opt_runs)
     print(len(opt_runs))
-    # opt_run = opt_runs[0]
+    opt_run = opt_runs[3]
     print(opt_run)
     opt_run.test_case.run()
     opt_run.run_bnd_cases()
