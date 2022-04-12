cd931aabfc30ea3bb34f2151a608cc78cdee3096
gmclove <george.love077@gmail.com>
2022-04-12 12:48:04 -0400

    test print

--- pymooCFD/core/optRun.py
+++ pymooCFD/core/optRun.py
@@ -96,6 +96,7 @@ class OptRun(PicklePath):
         #    Required Attributes    #
         #############################
         self.run_path = self.abs_path
+        print(algorithm)
         self.algorithm = algorithm.setup(problem,
                                          seed=algorithm.seed,
                                          verbose=algorithm.verbose,
