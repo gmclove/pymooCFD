1b73a3680ed352da9e0f02dc682c68ee4732d7a6
gmclove <george.love077@gmail.com>
2022-04-14 11:56:36 -0400

    .

---
+++ exec_comp_dist.py
@@ -0,0 +1,10 @@
+from pymooCFD.core.minimizeCFD import MinimizeCFD
+from pymooCFD.problems.comp_dist import CompDist
+
+def main():
+    study = MinimizeCFD(CompDist)
+
+
+
+if __name__ == '__main__':
+    main()
