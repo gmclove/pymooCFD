e1ce27e6441f3b8e755cd3c7a25a5963bed46efa
gmclove <george.love077@gmail.com>
2022-04-14 15:45:19 -0400

    .

--- pymooCFD/core/cfdCase.py
+++ pymooCFD/core/cfdCase.py
@@ -80,7 +80,6 @@ class CFDCase(PicklePath):  # (PreProcCase, PostProcCase)
         #    RESTART VARIABLES    #
         ###########################
         # self.complete = False
-        self.restart = False
         # self.parallelizeInit(self.externalSolver)
         self._x = x
 
@@ -112,6 +111,7 @@ class CFDCase(PicklePath):  # (PreProcCase, PostProcCase)
         #    Attributes    #
         ####################
         self.setup_parallelize(self.externalSolver)
+        self.restart = False
         self.validated = validated
         # Default Attributes
         if mesh_study is None:
