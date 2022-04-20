f3d68990bd9b5496d575b1690f779430e7b0744d
gmclove <george.love077@gmail.com>
2022-04-20 18:10:52 -0400

    .

--- exec_rans_const_opt.py
+++ exec_rans_const_opt.py
@@ -7,10 +7,10 @@ def main():
     # path = os.path.join(study.abs_path, 'best_k-e_standard')
     # RANS_k_eps(path, [0.09, 30_000]).run()
     xl = [0.09*0.8, 1_000]
-    xu = [0.09*1.2, 5_000]
+    xu = [0.09*1.2, 15_000]
     prob = study.get_problem(xl, xu)
-    alg = study.get_algorithm(n_gen=2, pop_size=10, n_offsprings=5)
-    opt_run = study.new_run(alg, prob, run_dir='short_run')
+    alg = study.get_algorithm(n_gen=20, pop_size=30, n_offsprings=4)
+    opt_run = study.new_run(alg, prob) #, run_dir='short_run')
     # print(study.get_logger())
     # print(study.opt_runs)
     # print(study.__dict__)
