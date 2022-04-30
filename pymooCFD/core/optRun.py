2739e60f0e8c73bc0591ec80dd8bf7be8fa337ed
gmclove <george.love077@gmail.com>
2022-04-30 13:32:27 -0400

    callback.gen -> len(alg.history)

--- pymooCFD/core/optRun.py
+++ pymooCFD/core/optRun.py
@@ -354,7 +354,7 @@ class OptRun(PicklePath):
 
     def plotGen(self, gen=None, max_leg_len=10):
         if gen is None:
-            gen = self.algorithm.callback.gen
+            gen = len(self.algorithm.history)  # self.algorithm.callback.gen
         pop = self.algorithm.history[gen - 1].pop
         if len(pop) <= max_leg_len:
             leg = True
