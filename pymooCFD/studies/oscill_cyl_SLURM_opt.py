c6e443d9cd8275b63945da0409976f0745aa7c8a
gmclove <george.love077@gmail.com>
2022-05-03 19:29:49 -0400

    m

---
+++ pymooCFD/studies/oscill_cyl_SLURM_opt.py
@@ -0,0 +1,37 @@
+from pymooCFD.core.minimizeCFD import MinimizeCFD
+from pymooCFD.problems.oscill_cyl import OscillCylinder_SLURM as BaseCase
+from pymooCFD.core.pymooBase import CFDTestProblem
+
+def exec_test():
+    study = MinimizeCFD(BaseCase, CFDGeneticProblem=CFDTestProblem)
+    run_dir = 'test_run'
+    if run_dir in study.opt_runs:
+        opt_run = study.opt_runs[run_dir]
+    else:
+        xl = [0.1, 0.2]  # lower limits of parameters/variables
+        xu = [10, 1]  # upper limits of variables
+        prob = study.get_problem(xl, xu)
+        alg = study.get_algorithm(n_gen=2, pop_size=3,
+                                  n_offsprings=2)
+        opt_run = study.new_run(alg, prob, run_dir=run_dir)
+    opt_run.test_case.run()
+    # opt_run.run_bnd_cases()
+    opt_run.run()
+
+def exec():
+    study = MinimizeCFD(BaseCase)
+    study.new_case('oscill_cyl_f=Strouhal')
+    run_dir = 'test_run'
+    if run_dir in study.opt_runs:
+        opt_run = study.opt_runs[run_dir]
+    else:
+        xl = [0.1, 0.2]  # lower limits of parameters/variables
+        xu = [10, 1]  # upper limits of variables
+        prob = study.get_problem(xl, xu)
+        alg = study.get_algorithm(n_gen=35, pop_size=50,
+                                  n_offsprings=15)
+        opt_run = study.new_run(alg, prob, run_dir=run_dir)
+    opt_run.test_case.run()
+    opt_run.test_case.mesh_study.run()
+    opt_run.run_bnd_cases(n_pts=3, doMeshStudy=True)
+    opt_run.run()
