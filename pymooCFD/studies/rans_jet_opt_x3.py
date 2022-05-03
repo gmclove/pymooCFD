d870ad3477f9c0010c7f5b7729869cf4257aecef
gmclove <47356727+gmclove@users.noreply.github.com>
2022-05-02 23:27:29 -0400

    bnd correct

--- pymooCFD/studies/rans_jet_opt_x3.py
+++ pymooCFD/studies/rans_jet_opt_x3.py
@@ -12,7 +12,7 @@ def exec_test(test_run_dir='test_run', **kwargs):
     else:
         alg = study.get_algorithm(n_gen=2, pop_size=3, n_offsprings=2)
         xl = [0.002, 0.1, 0.5]  # lower limits of parameters/variables
-        xu = [0.04, 0.6, 1.5]  # upper limits of variables
+        xu = [0.04, 0.6, 1]  # upper limits of variables
         prob = study.get_problem(xl, xu, **kwargs)
         opt_run = study.new_run(alg, prob, run_dir=test_run_dir, **kwargs)
     opt_run.test_case.run()
@@ -28,7 +28,7 @@ def exec_study(**kwargs):
         opt_run = study.opt_runs[run_dir]
     else:
         xl = [0.002, 0.1, 0.5]  # lower limits of parameters/variables
-        xu = [0.04, 0.6, 1.5]  # upper limits of variables
+        xu = [0.04, 0.6, 1]  # upper limits of variables
         alg = study.get_algorithm(n_gen=35, pop_size=30, n_offsprings=8)
         prob = study.get_problem(xl, xu)
         opt_run = study.new_run(alg, prob, run_dir=run_dir, **kwargs)
