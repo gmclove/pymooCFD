d796f4362415abef1c0d3db0a4454a613291a949
gmclove <george.love077@gmail.com>
2022-05-13 12:13:36 -0400

    coreection

--- pymooCFD/studies/oscill_cyl_SLURM_Re500_opt.py
+++ pymooCFD/studies/oscill_cyl_SLURM_Re500_opt.py
@@ -1,5 +1,5 @@
 from pymooCFD.core.minimizeCFD import MinimizeCFD
-from pymooCFD.problems.oscill_cyl import OscillCylinder_SLURM as BaseCase
+from pymooCFD.problems.oscill_cyl import OscillCylinder_SLURM_Re500 as BaseCase
 # from pymooCFD.core.pymooBase import CFDTestProblem
 
 def exec_test():
