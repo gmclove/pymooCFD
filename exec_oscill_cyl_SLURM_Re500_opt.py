0f8cf32efb3eeff83cde70c60ce71818cad61ce8
gmclove <george.love077@gmail.com>
2022-05-13 12:11:12 -0400

    ^

---
+++ exec_oscill_cyl_SLURM_Re500_opt.py
@@ -0,0 +1,23 @@
+from pymooCFD.studies.oscill_cyl_SLURM_Re500_opt import exec_test, exec_study, \
+                                                        BaseCase, MinimizeCFD
+
+
+def main():
+    # exec_test()
+    exec_study()
+    # study = MinimizeCFD(BaseCase)
+    # xl = [0.2, 0.1]  # lower limits of parameters/variables
+    # xu = [8, 1]  # upper limits of variables
+    # prob = study.get_problem(xl, xu)
+    # alg = study.get_algorithm(n_gen=25, pop_size=50,
+    # n_offsprings=15)
+    # opt_run = study.new_run(alg, prob)
+    # opt_run = study.opt_runs['test_run']
+    # opt_run.problem.BaseCase.procLim = 60
+    # opt_run.run_test_case()
+    # opt_run.run_bnd_cases()
+    # opt_run.run()
+
+
+if __name__ == '__main__':
+    main()
