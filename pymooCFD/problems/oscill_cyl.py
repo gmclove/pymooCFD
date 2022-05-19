cd19dab3236c16b42d9be70e13b56489ca046828
gmclove <george.love077@gmail.com>
2022-05-19 13:35:39 -0400

    move meshtool function into new module

--- pymooCFD/problems/oscill_cyl.py
+++ pymooCFD/problems/oscill_cyl.py
@@ -4,6 +4,7 @@ import os
 
 from pymooCFD.core.cfdCase import YALES2Case
 from pymooCFD.core.meshStudy import MeshStudy
+from pymooCFD.util.meshTools import get_coeff_and_NN
 
 
 class OscillCylinder(YALES2Case):
@@ -201,25 +202,25 @@ class OscillCylinder(YALES2Case):
         len_top = abs(bnds_top[1][0] - bnds_top[0][0])
         bnds_bot = gmsh.model.getParametrizationBounds(1, l_bot)
         len_bot = abs(bnds_bot[1][0] - bnds_bot[0][0])
-
-        def get_coeff_and_NN(x_min, x_max, x_tot, NN_init=100, coef_init=1.001):
-            max_it = 40
-            it = 0
-            thresh = 1e-6
-            err = np.inf
-
-            x_0 = x_min
-            x_f = x_max
-            NN = NN_init
-            coef = coef_init
-            while it < max_it and err > thresh:
-                coef_prev = coef
-                NN = int(np.log(1 + x_tot / x_0 * (coef - 1)) / np.log(coef) + 3)
-                coef = np.e**(np.log(x_f / x_0) / (NN - 3))
-                err = abs(coef_prev - coef)
-                it += 1
-                # print(it, err, coef, NN)
-            return coef, NN
+        
+        # def get_coeff_and_NN(x_min, x_max, x_tot, NN_init=100, coef_init=1.001):
+        #     max_it = 40
+        #     it = 0
+        #     thresh = 1e-6
+        #     err = np.inf
+        #
+        #     x_0 = x_min
+        #     x_f = x_max
+        #     NN = NN_init
+        #     coef = coef_init
+        #     while it < max_it and err > thresh:
+        #         coef_prev = coef
+        #         NN = int(np.log(1 + x_tot / x_0 * (coef - 1)) / np.log(coef) + 3)
+        #         coef = np.e**(np.log(x_f / x_0) / (NN - 3))
+        #         err = abs(coef_prev - coef)
+        #         it += 1
+        #         # print(it, err, coef, NN)
+        #     return coef, NN
 
         x_min = meshSizeMin
         x_max = meshSizeMax
