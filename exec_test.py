6674d32d00d9a048e0a75b9cea20d35146be1ed3
gmclove <47356727+gmclove@users.noreply.github.com>
2022-08-08 17:01:08 -0400

    clean up

---
+++ exec_test.py
@@ -0,0 +1,13 @@
+# from pymooFEA.studies.rans_const_opt import exec_test as test_rans_const_opt
+# from pymooFEA.studies.rans_jet_opt import exec_test as test_rans_jet_opt
+from pymooFEA.studies.oscill_cyl_opt import exec_test as test_oscill_cyl_opt
+
+
+def main():
+    # test_rans_const_opt()
+    # test_rans_jet_opt()
+    test_oscill_cyl_opt()
+
+
+if __name__ == '__main__':
+    main()
